7c39004ecba3a8c561f802a64c96f6eb45452724
Joshua Stauter <dthtvwls@users.noreply.github.com>
2022-01-23 01:44:36 -0500

    set -x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -l
 
-aws eks update-kubeconfig --name $1
+set -x
+
+aws eks update-kubeconfig --name "$1"
 helm template /etc/helm/charts/default-deployment --set "name=${2//\//-},namespace=$3,image=$4" | kubectl apply -f -
