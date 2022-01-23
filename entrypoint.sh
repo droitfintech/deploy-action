74f8741b1759f2b1fd7501c2466a8379dcc6b7f8
Joshua Stauter <dthtvwls@users.noreply.github.com>
2022-01-23 02:23:25 -0500

    could this be a problem?

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 #!/bin/sh -l
 
-set -x
-
-echo "$1"
-echo "$2"
-echo "$3"
-echo "$4"
-
 aws eks update-kubeconfig --name "$1"
-helm template /etc/helm/charts/default-deployment --set "name=${2//\//-},namespace=$3,image=$4" | kubectl apply -f -
+helm template /etc/helm/charts/default-deployment --set "name=$2,namespace=$3,image=$4" | kubectl apply -f -
