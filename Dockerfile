a4994b871cb48920f2f72b129503911a5e3f0895
Joshua Stauter <dthtvwls@users.noreply.github.com>
2022-01-25 09:24:02 -0500

    move entrypoint to image build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,1 @@
 FROM droit/deploy-action:v1
-
-COPY entrypoint.sh /entrypoint.sh
-ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-
-aws eks update-kubeconfig --name "$1"
-helm template /etc/helm/charts/default-deployment --set "name=$2,namespace=$3,image=$4" | kubectl apply -f -
-
-sleep 5
-
-hostname=$(kubectl get service $2 --namespace $3 --output json | jq ".status.loadBalancer.ingress[0].hostname")
-echo "::set-output name=hostname::$hostname"
